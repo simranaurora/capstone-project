ee3719c35a3b801a09440cbe3baef0c7a8076bcf
simranaurora <simran.aurora69@gmail.com>
2024-07-18 17:32:44 -0400

    typo fix

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api', contactRoute);
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
